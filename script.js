ad0356e402027c301f1e0eb89ccd2b45f27c5067
MaximGuschin <165547608+MaximGuschin@users.noreply.github.com>
2024-04-19 23:11:16 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let burger = document.querySelector(".burger-menu")
-let nav = document.querySelector(".main-nav")
-burger.addEventListener("click", function() {
-    burger.classList.toggle("active")
-    nav.classList.toggle("active")
-})
-
